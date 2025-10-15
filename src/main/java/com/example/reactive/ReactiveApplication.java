0df71161ec3ba5b862a525f2b99b7e42a8851b2f
aksenaksen <ans109905@naver.com>
2025-10-15 16:46:12 +0900

    yml 및 appliction basepackage수정

--- src/main/java/com/example/reactive/ReactiveApplication.java
+++ src/main/java/com/example/reactive/ReactiveApplication.java
@@ -2,8 +2,10 @@ package com.example.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.reactive.${sec}")
+@EnableR2dbcRepositories(basePackages = "com.example.reactive.${sec}")
 public class ReactiveApplication {
 
     public static void main(String[] args) {
