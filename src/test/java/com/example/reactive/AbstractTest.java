5894c07d56a7e540c72e6b7d6faa8c60ce8f1c5e
aksenaksen <ans109905@naver.com>
2025-10-15 17:43:16 +0900

    R2DBC SQL 로깅 추가

--- src/test/java/com/example/reactive/AbstractTest.java
+++ src/test/java/com/example/reactive/AbstractTest.java
@@ -2,7 +2,10 @@ package com.example.reactive;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "sec=sec02")
+@SpringBootTest(properties = {
+        "sec=sec02",
+        "logging.level.org.springframework.r2dbc=DEBUG"
+})
 public abstract class AbstractTest {
 
 }
