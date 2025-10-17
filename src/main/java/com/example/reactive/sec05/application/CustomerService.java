7406fa16262c6271770fb6eedf736ea487b6315d
aksenaksen <ans109905@naver.com>
2025-10-17 13:50:05 +0900

    5 섹션에 4 복사

---
+++ src/main/java/com/example/reactive/sec05/application/CustomerService.java
@@ -0,0 +1,53 @@
+package com.example.reactive.sec05.application;
+
+import com.example.reactive.sec05.dto.CustomerDto;
+import com.example.reactive.sec05.infrastructor.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public Flux<CustomerDto> getAllCustomers(){
+        return customerRepository.findAll()
+                .map(CustomerDto::fromEntity);
+    }
+
+    public Flux<CustomerDto> getAllCustomers(Integer page, Integer size){
+        return customerRepository.findBy(PageRequest.of(page - 1 , size))
+                .map(CustomerDto::fromEntity);
+    }
+
+    public Mono<CustomerDto> getCustomerById(Integer id){
+        return customerRepository.findById(id)
+                .map(CustomerDto::fromEntity);
+    }
+
+    public Mono<CustomerDto> save(Mono<CustomerDto> mono){
+        return mono.map(CustomerDto::toEntity)
+                .flatMap(customerRepository::save)
+                .map(CustomerDto::fromEntity);
+    }
+
+    public Mono<CustomerDto> update(Integer id, Mono<CustomerDto> mono){
+        return customerRepository.findById(id)
+                .flatMap(entity -> mono)
+                .map(CustomerDto::toEntity)
+                .doOnNext(c -> c.setId(id))
+                .flatMap(customerRepository::save)
+                .map(CustomerDto::fromEntity);
+    }
+
+    public Mono<Boolean> delete(Integer id) {
+        return customerRepository.deleteCustomerById(id);
+    }
+
+
+
+}
---
+++ src/main/java/com/example/reactive/sec05/domain/CustomerOrder.java
@@ -0,0 +1,29 @@
+package com.example.reactive.sec05.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Table("customer_order")
+@Getter
+@Setter
+@ToString
+public class CustomerOrder {
+
+    @Id
+    private UUID id;
+
+    private Integer customerId;
+
+    private Integer productId;
+
+    private Integer amount;
+
+    private Instant orderDate;
+
+}
