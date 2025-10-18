6484698733e2b87184fc610c0fb148ad7d65e55f
aksenaksen <ans109905@naver.com>
2025-10-18 14:25:37 +0900

    SinkConfig 추가 - Sink = 입구

---
+++ src/main/java/com/example/reactive/sec07/config/SinkConfig.java
@@ -0,0 +1,15 @@
+package com.example.reactive.sec07.config;
+
+import com.example.reactive.sec06.dto.ProductDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Sinks;
+
+@Configuration
+public class SinkConfig {
+
+    @Bean
+    public Sinks.Many<ProductDto> sinks(){
+        return Sinks.many().replay().limit(1);
+    }
+}
