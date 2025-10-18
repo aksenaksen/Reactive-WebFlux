2a8187b4a990af68d5e6bb91af49cea099686a92
aksenaksen <ans109905@naver.com>
2025-10-18 17:07:20 +0900

    커넥션 풀링 테스트

---
+++ src/test/java/com/example/reactive/sec08/ConnectionPoolingTest.java
@@ -0,0 +1,56 @@
+package com.example.reactive.sec08;
+
+import com.example.reactive.sec06.dto.ProductDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class ConnectionPoolingTest extends AbstractWebClient{
+
+    private final WebClient webClient = createWebClient(b -> {
+        var poolSize = 1000;
+        var provider = ConnectionProvider.builder("vins")
+                .lifo()
+                .maxConnections(poolSize)
+                .pendingAcquireMaxCount(poolSize * 5)
+                .build();
+
+        var httpClient = HttpClient.create(provider)
+                .compress(true)
+                .keepAlive(true);
+        b.clientConnector(new ReactorClientHttpConnector(httpClient));
+    });
+// WebClient 는 최대 500개까지만 요청연결 가능.
+
+    @Test
+    @DisplayName("")
+    void ConcurrentRequestTest() {
+        //given
+        var max = 501;
+        Flux.range(1, max)
+                .flatMap(this::getProduct, max)
+                .collectList()
+                .as(StepVerifier::create)
+                .assertNext(list -> Assertions.assertEquals(list.size(), max))
+                .verifyComplete();
+        //when
+        //then
+    }
+
+
+    private Mono<ProductDto> getProduct(Integer id){
+        return webClient.get()
+                .uri("/product/{id}", id)
+                .retrieve()
+                .bodyToMono(ProductDto.class);
+    }
+}
